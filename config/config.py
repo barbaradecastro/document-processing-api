5f06cbd5ccb332c6ca4c169c0f8b24ff0f3ae83b
barbaradecastro <barbaradecastro82@yahoo.com.br>
2026-01-22 18:34:32 -0300

    Initial commit - document processing API

---
+++ config/config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BUCKET_NAME = os.getenv("")
+REGION_NAME = os.getenv("")
+AWS_ACCESS_KEY_ID = os.getenv("")
+AWS_SECRET_ACCESS_KEY = os.getenv("")
+AWS_SESSION_TOKEN = os.getenv("")
---
+++ service/unzip_service.py
@@ -0,0 +1,13 @@
+import zipfile
+import io
+
+def extract_files_from_zip(zip_bytes):
+    imagens = []
+    with zipfile.ZipFile(io.BytesIO(zip_bytes)) as zip_file:
+        for name in zip_file.namelist():
+            with zip_file.open(name) as file:
+                imagens.append({
+                    "filename": name,
+                    "content": file.read()
+                })
+    return imagens
